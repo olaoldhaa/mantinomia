07486e44c00d50f4759447bc3d7d071ae6455916
Ola Oldernes Hårstad <haarstad.ola@gmail.com>
2023-03-21 12:02:04 +0100

    Fixed paragraph spaces

--- script.js
+++ script.js
@@ -2,12 +2,15 @@
 const text = document.getElementById("text");
 
 // Fetch the contents of the text file "mantinomia.txt" and return a Promise
-fetch("./mantinomia.txt")
+fetch("mantinomia.txt")
   // When the Promise is resolved, convert the response to text using the "text()" method
   .then((response) => response.text())
-  // When the text data is available, set the "innerHTML" property of the "text" element to the text data
-  .then((data) => {
-    text.innerHTML = data;
+  // When the text data is available, replace paragraph spaces with <br><br> using a regular expression
+  .then((contents) => {
+    const replacedContents = contents.replace(/\n\s*\n/g, "<br><br>");
+
+    // Set the "innerHTML" property of the "text" element to the replaced text data
+    text.innerHTML = replacedContents;
   })
   // If there is an error, log the error message to the console
   .catch((error) => console.log(error));
